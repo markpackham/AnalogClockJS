a56a2739ccf5d396b3af8550ed4eff7a4d27b692
mark <markpackham1@gmail.com>
2022-08-22 14:24:57 +0100

    start hand rotation, needed to use ids instead of classes for style.transform
    https://youtu.be/hbe2hn4W3z0?t=960

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-const secondsHand = document.getElementsByClassName("seconds-hand");
-const minutesHand = document.getElementsByClassName("minutes-hand");
-const hoursHand = document.getElementsByClassName("hours-hand");
+const secondsHand = document.getElementById("seconds-hand");
+const minutesHand = document.getElementById("minutes-hand");
+const hoursHand = document.getElementById("hours-hand");
 
 function getTime() {
   const now = new Date();
   const seconds = now.getSeconds();
   const minutes = now.getMinutes();
   const hours = now.getHours();
+  const timeInterval = 6;
+
+  secondsHand.style.transform = "rotate(60deg)";
 }
+
+getTime();
